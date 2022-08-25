ab9e9d84fbdc51c28d38223c4a11dac6e499758f
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-08-25 17:28:09 +0500

    initial commit

---
+++ src/main/java/uz/everbestlab/tasbehapi/TasbehApiApplication.java
@@ -0,0 +1,13 @@
+package uz.everbestlab.tasbehapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasbehApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasbehApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/everbestlab/tasbehapi/TasbehApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.everbestlab.tasbehapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasbehApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
